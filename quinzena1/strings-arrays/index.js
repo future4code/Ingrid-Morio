27428aaffaa9cb027c0f3ff2b3063e18db8bdac9
Ingrid Morio <ingrid.sanae@gmail.com>
2021-07-22 20:28:11 -0400

    exercicios aula 05

---
+++ quinzena1/strings-arrays/index.js
@@ -0,0 +1,52 @@
+// Exercício interpretação
+
+/*
+1) Respostas:
+a. undefined
+b. null
+c. 11
+d. 3
+e. sbutitui o valor do item 1. Ou seja, 4 por 19
+f. informa o valor da sexta posição
+
+2) SUBI NUM ÔNIBUS EM MIRROCOS, 27
+
+*/
+
+// Exercícios escrita
+
+// Atividade 1:
+
+// let nomeDoUsuario = prompt("Digite seu nome:")
+// let emailDoUsuario = prompt("Digite seu e-mail:")
+
+// console.log("O e-mail" + " " + emailDoUsuario + " " + "foi cadastrado com sucesso. Seja bem-vinda(o)," + " "+ nomeDoUsuario)
+
+// Atividade 2:
+
+// let listaComidasFavoritas = ['macarrão', 'lasanha', 'comida japonesa', 'salgadinho', 'bolo']
+// listaComidasFavoritas.forEach(function (novo) {
+// console.log(novo)
+
+// })
+
+// let novaComida = prompt("Informe uma nova comida favorita:")
+// listaComidasFavoritas[1] = novaComida
+// console.log(listaComidasFavoritas)  
+
+// Atividade 3:
+
+// let listaDeTarefas = []
+// let tarefa1 = prompt("Informe a 1ª tarefa:")
+// let tarefa2 = prompt("Informe a 2ª tarefa:")
+// let tarefa3 = prompt("Informe a 3ª tarefa:")
+
+// listaDeTarefas = [tarefa1, tarefa2, tarefa3]
+// console.log(listaDeTarefas)
+
+// let tarefaRealizada =  Number(prompt("Informe a tarefa realizada, utilizando os números: 0 - " + tarefa1 + ", " + "1 - "+ tarefa2 + ", " + "2 - " + tarefa3))
+// listaDeTarefas.splice(tarefaRealizada, 1)
+// console.log(listaDeTarefas)
+
+
+
