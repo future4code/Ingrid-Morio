c73b8a7526e18850c3d08ba4055f3e15ea3f7c6a
Ingrid Morio <ingrid.sanae@gmail.com>
2021-07-27 22:11:25 -0400

    exercicios aula 06

---
+++ quinzena2/funcoes/index.js
@@ -0,0 +1,108 @@
+// Exercício 1 letra A
+
+// function dados(){
+//     let nome = "Ingrid"
+//     let idade = 29
+//     let cidade = "Cuiabá"
+//     let profissao = "assistente administrativo"
+//     let imprimir = `Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`
+// console.log(imprimir)
+
+// }
+// dados()
+
+// Exercício 1 letra B
+
+// function imprimirDados (nomeUsuario, idadeUsuario, cidadeUsuario, profissaoUsuario){
+// const mensagem = `Eu sou ${nomeUsuario}, tenho ${idadeUsuario} anos, moro em ${cidadeUsuario} e sou ${profissaoUsuario}.`
+// console.log(mensagem)
+// }
+// imprimirDados("Ingrid", 29, "Cuiabá", "assistente administrativo")
+
+// Exercício 2 letra A
+
+// function calculo(num1, num2){
+// const soma = num1 + num2
+// return soma
+// }
+// const dadosCalculados = calculo(8, 7)
+// console.log(calculo(8, 7))
+
+// Exercício 2 letra B
+
+// function comparacao(primeiroNumero, segundoNumero){
+// const verificar = primeiroNumero >= segundoNumero 
+// console.log(verificar)
+// }
+// comparacao(3,9)
+
+// Exercício 2 letra C
+
+// function numeroPar(informaNumero){
+// const analise = informaNumero % 2 == 0
+// console.log("O número é par? ", analise)
+// }
+// numeroPar(99)
+
+// Exercício 2 letra D
+
+// function recado(texto){
+// const recadoMaiusculo = texto.toUpperCase()
+// const tamanhoRecado = texto.length
+// console.log(`O tamanho da mensagem é: ${tamanhoRecado}. Mensagem em maiúsculo: ${recadoMaiusculo}`)
+
+// }
+// recado("Gosto de comer chocolate")
+
+// // Exercício 3 
+
+// const dados1 = Number(prompt("Insira um número:"))
+// const dados2 = Number(prompt("Insira outro número:"))
+
+// function calculoAdicao(){
+    
+//     const adicao = dados1 + dados2
+//     console.log("Soma: ", adicao) 
+   
+// }
+// calculoAdicao()
+
+
+// function calculoSubtracao(){
+//     const subtracao = dados1 - dados2
+//     console.log("Subtração: ", subtracao) 
+
+// }
+// calculoSubtracao()
+
+// function calculoMultiplicacao(){
+//     const multiplicacao = dados1 * dados2
+//     console.log("Multiplicação: ", multiplicacao)
+
+// }
+// calculoMultiplicacao()
+
+// function calculoDivisao(){
+//     const divisao = dados1 / dados2
+//     console.log("Divisão: ", divisao) 
+
+// }
+// calculoDivisao()
+
+
+// Desafio n° 1 letra A
+
+// const dadosArrow = (info) => {
+// const consolidado = info
+// return consolidado
+
+// }
+
+// const final = dadosArrow(5)
+// console.log(final)
+
+
+
+
+
+
